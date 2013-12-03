125141f3a9112ba641ea5b4a2430e8c89021b875
pgaref@ubuntu <pgaref@pgaref-OptiPlex-755>
2013-12-03 20:46:59 +0200

    aaa

--- src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java
+++ src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java
@@ -275,7 +275,7 @@ public class QuorumPeerMain {
 						 * A real zookeeper would take care of setting the
 						 * current vote. Here we do it manually.
 						 */
-						quorumPeer.cnxnFactory.shutdown();
+						quorumPeer.stopLeaderElection();
 						quorumPeer.setCurrentVote(currentVote);
 
 						quorumPeer.startLeaderElection();
